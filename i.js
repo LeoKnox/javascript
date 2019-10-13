ac085f016ba811e28bb8bd7ba4e6806a73e40719
LeoKnox <LeoKnox@users.noreply.github.com>
2019-10-12 22:57:48 -0700

    Add files via upload

--- i.js
+++ i.js
@@ -1,4 +1,4 @@
-document.getElementById("iform").innerHTML="<input type='text'></input><input type='button'>click</input>";
+document.getElementById("iform").innerHTML="<input type='text'></input><input type='submit'>";
 
 function runform() {
   document.getElementById("runner").innerHTML="running";
